a0f3fcedeac82f89ea08dadb7c1731f906d7f84c
icom85 <h.merdjanov@gmail.com>
2014-06-01 16:17:28 +0300

    Added main class

---
+++ BattleFieldMain.cs
@@ -0,0 +1,20 @@
+﻿namespace BattleFiled
+{
+    using System;
+
+    class BattleFieldMain
+    {
+        static Engine GetEngineInstance()
+        {
+            return new Engine();
+        }
+
+        static void Main()
+        {
+            Engine gameEngine = GetEngineInstance();
+
+            gameEngine.Run();
+        }
+    }
+}
+
