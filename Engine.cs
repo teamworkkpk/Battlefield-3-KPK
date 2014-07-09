49ba3d85bbbe8fda690f1d7ba514bbc67329a4dc
Georgi Neykov <georgi.neykov@gmail.com>
2014-07-10 01:08:09 +0300

    Added key events to Engine class, refacored it abit. To subscribe for an event, use Engine.AddArrowKeyPressedHandler(...) Added new class KeyEventArgs to be used with the new events, main purpose is to pass info to event subscribers what key was pressed, may be extended to hold more data about the event.

--- Engine.cs
+++ Engine.cs
@@ -11,19 +11,122 @@
         //{
         //    this.sizeOfField = sizeOfField;
         //}
-
         public Engine()
         {
-           
+        }
+  
+        /// <summary>
+        /// Occurs when an arrow keypress is detected.
+        /// </summary>
+        private event EventHandler<KeyEventArgs> ArrowKeyPressed;
+
+        private Render renderer;
+
+        private bool keepRunning;
+        private bool isRunning;
+
+        /// <summary>
+        /// Adds an event handler to the <see cref="E:ArrowKeyPressed"/> event.
+        /// </summary>
+        /// <param name="handler">The handler to be added</param>
+        /// <exception cref="ArgumentNullException">null <paramref name="handler"/> argument.</exception>
+        public void AddArrowKeyPressedHandler(EventHandler<KeyEventArgs> handler) 
+        {
+            if (handler == null) 
+            {
+                throw new ArgumentNullException("Null 'handler' argument.");
+            }
+
+            this.ArrowKeyPressed += handler;
         }
 
-        //public int SizeOfField
-        //{
-        //    get
-        //    {
-        //        return this.sizeOfField;
-        //    }
-        //}
+        /// <summary>
+        /// Removes an event handler attached to the <see cref="E:ArrowKeyPressed"/> event.
+        /// </summary>
+        /// <param name="handler">The handler to be removed</param>
+        /// <exception cref="ArgumentNullException">null <paramref name="handler"/> argument.</exception>
+        public void RemoveArrowKeyPressedHandler(EventHandler<KeyEventArgs> handler) 
+        {
+            if (handler == null) 
+            {
+                throw new ArgumentNullException("Null 'handler' argument.");
+            }
+
+            this.ArrowKeyPressed -= handler;
+        }
+
+
+        public void Start()
+        {
+            if (!isRunning)
+            {
+                this.keepRunning = true;
+
+                this.Run();
+            }
+        }
+
+        public void Stop() 
+        {
+            this.keepRunning = false;
+        }
+
+        private void Run() 
+        {
+            if (this.renderer == null)
+            {
+                this.renderer = new Render();
+            }
+
+            int sizeOfField = this.ReadSize();
+            Playfield field = InitializeField(sizeOfField);
+            List<IGameObject> gameObjects = new List<IGameObject>();
+            gameObjects.Add(field);
+            this.renderer.DrawGameElements(gameObjects);
+            // TODO Render Field, Handle Exceptions...
+
+            while (keepRunning)
+            {
+                this.isRunning = true;
+
+                //Check if any keys where pressed
+                ConsoleKey pressedKey;
+                if (Console.KeyAvailable)
+                {
+                    pressedKey = Console.ReadKey().Key;
+                    switch (pressedKey)
+                    { 
+                        //TODO: Add move key events, maybe for <Enter>
+                        case ConsoleKey.LeftArrow:
+                        case ConsoleKey.RightArrow:
+                        case ConsoleKey.UpArrow:
+                        case ConsoleKey.DownArrow:
+                            KeyEventArgs args = new KeyEventArgs(pressedKey);
+                            this.OnArrowKeyPressed(args);
+                            break;
+                    }
+                    
+                    //Clear any pending keypresses from the inputstream.
+                    while (Console.KeyAvailable)
+                    {
+                        Console.ReadKey(true);
+                    }
+                }
+            }
+            this.isRunning = false;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:ArrowKeyPressed" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="BattleFiled.KeyEventArgs" /> instance containing the event data.</param>
+        protected virtual void OnArrowKeyPressed(KeyEventArgs e)
+        {
+            if (this.ArrowKeyPressed != null)
+            {
+                this.ArrowKeyPressed(this, e);
+            }
+        }
 
         private int ReadSize()
         {
@@ -43,22 +146,6 @@
             field.PlaceMines();
 
             return field;
-        }
-
-        public virtual void Run()
-        {
-            int sizeOfField = ReadSize();
-            List<IGameObject> gameObjects = new List<IGameObject>();
-            Playfield field = InitializeField(sizeOfField);
-            Render render = new Render();
-            gameObjects.Add(field);
-            render.DrawGameElements(gameObjects);
-            // TODO Render Field, Handle Exceptions...
-
-            while (true)
-            {
-                // Game Logic...
-            }            
         }
     }
 }
---
+++ KeyEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleFiled
+{
+    using System;
+
+    class KeyEventArgs : EventArgs
+    {
+        private readonly ConsoleKey pressedKey;
+
+        public KeyEventArgs(ConsoleKey pressedKey)
+        {
+            this.pressedKey = pressedKey;
+        }
+    }
+}
