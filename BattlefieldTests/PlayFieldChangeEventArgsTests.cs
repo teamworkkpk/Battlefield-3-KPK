1888faa0a8cdbd3af922965f5ba1329573a1465a
TanyaPetkova <taniapetkova@abv.bg>
2014-07-22 16:22:38 +0300

    Added PlayfieldChangedEventArgs Test
    100% coverage

---
+++ BattlefieldTests/PlayFieldChangeEventArgsTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BattleFiled;
+using BattleFiled.Cells;
+using BattleFiled.SaveLoad;
+using BattleFiled.GameEngine;
+
+namespace BattlefieldTests
+{
+    [TestClass]
+    public class PlayFieldChangeEventArgsTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "Null Playfield provided.")]
+        public void PlayFieldChangeEventArgsTestIfAcceptNull()
+        {
+            string message = string.Empty;
+
+            Playfield playfield = Playfield.Instance;
+            PlayfieldChangedEventArgs pl = new PlayfieldChangedEventArgs(null);
+        }
+
+        [TestMethod]
+        public void PlayFieldChangeEventArgsTestGetPlayFieldSize()
+        {
+            Playfield playfield = Playfield.Instance;
+            playfield.SetFieldSize(10);
+            PlayfieldChangedEventArgs pl = new PlayfieldChangedEventArgs(playfield);
+            Assert.AreEqual(pl.PlayfieldSize, 10);
+        }
+
+        [TestMethod]
+        public void PlayFieldChangeEventArgsTestGetNewPlayField()
+        {
+            Playfield playfield = Playfield.Instance;
+            playfield.SetFieldSize(10);
+            PlayfieldChangedEventArgs pl = new PlayfieldChangedEventArgs(playfield);
+            Assert.AreEqual(pl.NewPlayField, playfield);
+
+        }
+    }
+}
