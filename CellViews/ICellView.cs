bc69069c000d0374f62259b3ff46a07aaf0a5e11
tsigularoff <tsigularoff@gmail.com>
2014-07-22 16:47:54 +0300

    StyleCop: ICellView.cs

--- CellViews/ICellView.cs
+++ CellViews/ICellView.cs
@@ -1,10 +1,21 @@
-﻿namespace BattleFiled.CellViews
-{
-    using BattleFiled.Renderer.Context;
-    using System;
+﻿// <copyright file="ICellView.cs" company="Team Battlefield 3">
+// All rights reserved.
+// </copyright>
+// <author>Team Battlefield 3</author>
 
+namespace BattleFiled.CellViews
+{
+    using System;
+    using BattleFiled.Renderer.Context;    
+
+    /// <summary>
+    /// Interface that sets the methods for cell rendering.
+    /// </summary>
     public interface ICellView
     {
+        /// <summary>
+        /// Sets the way of cell rendering.
+        /// </summary>                
         void Draw();
     }
 }
