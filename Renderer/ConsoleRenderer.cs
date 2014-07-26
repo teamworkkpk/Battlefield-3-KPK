ac8a0018d350c687d6a1f50c435a3592a77a0dcc
Georgi Neykov <georgi.neykov@gmail.com>
2014-07-27 00:08:45 +0300

    StyleCop: ConsoleRenderer and Renderer. Introduced some properties to take down the warnings.

--- Renderer/ConsoleRenderer.cs
+++ Renderer/ConsoleRenderer.cs
@@ -1,38 +1,65 @@
-﻿namespace BattleFiled.Renderer
+﻿// <copyright file="ConsoleRenderer.cs" company="Team Battlefield 3">
+// All rights reserved.
+// </copyright>
+// <author>Team Battlefield 3</author>
+
+namespace BattleFiled.Renderer
 {
     using System;
-    using BattleFiled.CellViews;
     using BattleFiled.Cells;
+    using BattleFiled.CellViews;
     using BattleFiled.GameEngine;
 
+    /// <summary>
+    /// A concrete implementation of <see cref="Renderer"/> abstract class that uses the system's console
+    /// as a rendering environment.
+    /// </summary>
     public class ConsoleRenderer : Renderer
     {
+        /// <summary>
+        /// The numbers of symbols to be applied as padding on top/left sides of playing field.
+        /// </summary>
         private const int ConsolePadding = 5;
         
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleRenderer" /> class.
+        /// </summary>
+        /// <param name="engine">The engine object to be attached to this object instance.</param>
         public ConsoleRenderer(Engine engine) : base(engine)
         {
             Console.CursorVisible = false;
         }
 
+        /// <summary>
+        /// Initiates a procedure defined by concrete classes for drawing a game over screen.
+        /// </summary>
+        /// <param name="totalMoves">The total moves a player has made up to this moment.</param>
         public override void DrawGameOver(int totalMoves)
         {
             Console.SetCursorPosition(0, 0);
             Console.WriteLine("Game over, score {0}", totalMoves);
         }
 
+        /// <summary>
+        /// Initiates a draw procedure on controlled <see cref="ICellView" /> objects.
+        /// </summary>
         public override void DrawAll()
         {
             Console.Clear();
             base.DrawAll();
         }
 
+        /// <summary>
+        /// Concrete implementations should
+        /// implement this method according to their rendering environment.
+        /// </summary>
         protected override void DrawPointer()
         {
-            Console.SetCursorPosition(this.engine.Pointer.X + ConsolePadding, this.engine.Pointer.Y + ConsolePadding);
+            Console.SetCursorPosition(this.Engine.Pointer.X + ConsolePadding, this.Engine.Pointer.Y + ConsolePadding);
             Console.BackgroundColor = ConsoleColor.DarkYellow;
             Console.ForegroundColor = ConsoleColor.Yellow;
 
-            char symbol = (char)engine.PlayField[this.engine.Pointer.X, this.engine.Pointer.Y].CellView;
+            char symbol = (char)Engine.PlayField[this.Engine.Pointer.X, this.Engine.Pointer.Y].CellView;
 
             if (symbol != '0')
             {
@@ -46,13 +73,21 @@
             Console.ResetColor();
         }
 
+        /// <summary>
+        /// A method for creating new <see cref="ICellView" /> derived classes. Concrete implementations should
+        /// implement this method and return the proper type of object for their rendering environment.
+        /// This method is used by <see cref="Renderer.CreateCellViews" /> to get the right type of object needed.
+        /// </summary>
+        /// <param name="cell">The <see cref="ICell" /> object that will get displayed by returned object.</param>
+        /// <param name="isBackgroundChanged">A boolean.</param>
+        /// <returns>CellView object to be added to Renderer's list of controlled objects.</returns>
         protected override ICellView CreateCellView(ICell cell, bool isBackgroundChanged)
         {
             ICellView view;
             switch (cell.CellType)
             { 
                 case CellType.Bomb:
-                    view = new ConsoleView(cell.X, cell.Y, ConsoleColor.Red, isBackgroundChanged?ConsoleColor.Green:ConsoleColor.Blue, (char)cell.CellView);
+                    view = new ConsoleView(cell.X, cell.Y, ConsoleColor.Red, isBackgroundChanged ? ConsoleColor.Green : ConsoleColor.Blue, (char)cell.CellView);
                     break;
                 case CellType.BlownCell:
                     view = new ConsoleView(cell.X, cell.Y, ConsoleColor.Red, ConsoleColor.Gray, '*');
--- Renderer/Renderer.cs
+++ Renderer/Renderer.cs
@@ -1,39 +1,42 @@
-﻿namespace BattleFiled.Renderer
+﻿// <copyright file="Renderer.cs" company="Team Battlefield 3">
+// All rights reserved.
+// </copyright>
+// <author>Team Battlefield 3</author>
+
+namespace BattleFiled.Renderer
 {
     using System;
     using BattleFiled.Cells;
-    using BattleFiled.GameEngine;
     using BattleFiled.CellViews;
+    using BattleFiled.GameEngine;
     using BattleFiled.Renderer.Context;
     
     /// <summary>
-    /// Abstract class defining the behavior and interaction with an 
-    /// <see cref="GameEngine" />class.
+    /// Abstract class defining the behavior and interaction with an <see cref="GameEngine" />class.
     /// Renderer-derived classes respond to events fired by the Engine object they are
     /// explicitly attached by changing the state of <see cref="ICellView" /> objects they control.
     /// This class only binds event handlers and some common behavior, concrete classes
     /// should provide implementation or redefine it according to the rendering context 
-    /// (Console, Windows Forms, WPF, OpenGL, DirectX and etc.)
+    /// (Console, Windows Forms, WPF, OpenGL, DirectX and etc.).
     /// </summary>
     public abstract class Renderer : RenderingContext
     {
         /// <summary>
-        /// A reference to the <see cref="BattleFiled.GameEngine.GameEngine"/> object this Renderer is attached to.
+        /// A reference to the <see cref="Engine"/> object this Renderer is attached to.
         /// </summary>
-        protected readonly Engine engine;
+        private readonly Engine engine;
 
         /// <summary>
         /// A container for holding and accessing all controlled ICellView objects.
         /// </summary>
-        protected ICellView[,] cellViews;
+        private ICellView[,] cellViews;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Renderer" /> class.
         /// </summary>
-        /// <param name="engine">The engine.</param>
+        /// <param name="engine">The engine object to be attached to this object instance.</param>
         protected Renderer(Engine engine)
         {
-            
             if (engine == null)
             {
                 throw new ArgumentNullException("Null Engine provided.");
@@ -47,22 +50,55 @@
                 this.DrawAll();
             }
 
-            //Register event handlers.
-            this.engine.CurrentCellChanged += this.OnCurrentCellChangedHandler;
-            this.engine.CellsInRegionChanged += this.OnCellsInRegionChangedHandler;
-            this.engine.PlayfieldChanged += this.OnPlayfieldChangedHandler;
-            this.engine.CellsInRegionRedefined += this.OnCellsInRegionRedefinedHandler;
-            this.engine.CellRedefined += this.OnCellRedefinedHandler;
-            this.engine.CellChanged += this.OnCellChangedHandler;
+            // Register event handlers.
+            this.Engine.CurrentCellChanged += this.OnCurrentCellChangedHandler;
+            this.Engine.CellsInRegionChanged += this.OnCellsInRegionChangedHandler;
+            this.Engine.PlayfieldChanged += this.OnPlayfieldChangedHandler;
+            this.Engine.CellsInRegionRedefined += this.OnCellsInRegionRedefinedHandler;
+            this.Engine.CellRedefined += this.OnCellRedefinedHandler;
+            this.Engine.CellChanged += this.OnCellChangedHandler;
         }
-  
 
         /// <summary>
-        /// Initiates a draw procedure on controlled <see cref="ICellView"/> ibjects.
+        /// Gets the engine reference to the <see cref="Engine"/> object this Renderer is attached to.
+        /// </summary>
+        /// <value>The engine.</value>
+        protected Engine Engine
+        {
+            get
+            {
+                return this.engine;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets all controlled ICellView objects.
+        /// </summary>
+        /// <value>The cell views.</value>
+        protected ICellView[,] CellViews
+        {
+            get
+            {
+                return this.cellViews;
+            }
+            
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Null CellViews provided");
+                }
+
+                this.cellViews = value;   
+            }
+        }
+
+        /// <summary>
+        /// Initiates a draw procedure on controlled <see cref="ICellView"/> objects.
         /// </summary>
         public virtual void DrawAll()
         {
-            foreach (ICellView view in this.cellViews)
+            foreach (ICellView view in this.CellViews)
             {
                 view.Draw();
             }
@@ -71,7 +107,7 @@
         }
 
         /// <summary>
-        /// Initiates a procedure defined by conrete classes for drawing a game over screen.
+        /// Initiates a procedure defined by concrete classes for drawing a game over screen.
         /// </summary>
         /// <param name="totalMoves">The total moves a player has made up to this moment.</param>
         public abstract void DrawGameOver(int totalMoves);
@@ -81,9 +117,9 @@
         /// implement this method and return the proper type of object for their rendering environment.
         /// This method is used by <see cref="Renderer.CreateCellViews"/> to get the right type of object needed.
         /// </summary>
-        /// <param name="cell">The <see cref="ICell" object that will get displayed by returned object./></param>
+        /// <param name="cell">The <see cref="ICell"/> object that will get displayed by returned object.</param>
         /// <param name="shouldChangeColor">Color of the should change.</param>
-        /// <returns>CellView object to be added to Renderer's list of controlled objects</returns>
+        /// <returns>CellView object to be added to Renderer's list of controlled objects.</returns>
         protected abstract ICellView CreateCellView(ICell cell, bool shouldChangeColor);
 
         /// <summary>
@@ -93,13 +129,13 @@
         protected abstract void DrawPointer();
         
         /// <summary>
-        /// Called when a new <see cref="BattleFiled.PlayField"/> object is inserted to the attached GameEngine object.
+        /// Called when a new <see cref="Playfield"/> object is inserted to the attached GameEngine object.
         /// </summary>
         /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="BattleFiled.PlayfieldChangedEventArgs" /> instance containing the event data.</param>
+        /// <param name="e">The <see cref="PlayfieldChangedEventArgs" /> instance containing the event data.</param>
         protected virtual void OnPlayfieldChangedHandler(object sender, PlayfieldChangedEventArgs e)
         {
-            this.cellViews = this.CreateCellViews(e.NewPlayField);
+            this.CellViews = this.CreateCellViews(e.NewPlayField);
             this.DrawAll();
         }
 
@@ -112,7 +148,7 @@
         /// instance containing the event data about the changed CellView.</param>
         protected virtual void OnCellChangedHandler(object sender, CellEventArgs e)
         {
-            this.cellViews[e.Target.X, e.Target.Y].Draw();
+            this.CellViews[e.Target.X, e.Target.Y].Draw();
         }
   
         /// <summary>Called when there is a new ICell object being controlled by the attached GameEngine object. The default behavior is to create a new 
@@ -123,7 +159,7 @@
         protected virtual void OnCellRedefinedHandler(object sender, CellEventArgs e)
         {
             ICellView view = this.CreateCellView(e.Target, false);
-            this.cellViews[e.Target.X, e.Target.Y] = view;
+            this.CellViews[e.Target.X, e.Target.Y] = view;
             view.Draw();
         }
 
@@ -139,7 +175,7 @@
             int startY = e.RegionStartY;
             int endX = e.RegionEndX;
             int endY = e.RegionEndY;
-            Playfield playfield = this.engine.PlayField;
+            Playfield playfield = this.Engine.PlayField;
             bool shouldChangeColor = false;
 
             for (int indexX = startX; indexX < endX; indexX++)
@@ -152,13 +188,12 @@
                 for (int indexY = startY; indexY < endY; indexY++)
                 {
                     ICellView view = this.CreateCellView(playfield[indexX, indexY], shouldChangeColor);
-                    this.cellViews[indexX, indexY] = view;
+                    this.CellViews[indexX, indexY] = view;
                     view.Draw();
                     shouldChangeColor = !shouldChangeColor;
                 }
             }
         }
-
 
         /// <summary>
         /// Called when a group of ICell objects controlled by the attached GameEngine object change their state. The default behavior is to redraw the 
@@ -177,13 +212,13 @@
             { 
                 for (int indexY = startY; indexY < endY; indexY++)
                 {
-                    this.cellViews[indexX, indexY].Draw();
+                    this.CellViews[indexX, indexY].Draw();
                 }
             }
         }
 
         /// <summary>
-        /// Called when the Cell object on focus gets changed
+        /// Called when the Cell object on focus gets changed.
         /// </summary>
         /// <param name="sender">The sender.</param>
         /// <param name="e">The <see cref="BattleFiled.GameEngine.CellEventArgs" /> instance containing the event data.</param>
@@ -192,10 +227,10 @@
         }
 
         /// <summary>
-        /// Creates the ICellView object instances needed in order to visualize a provided <see cref="Battlefiled.Playfield"/> object.
+        /// Creates the ICellView object instances needed in order to visualize a provided <see cref="Playfield"/> object.
         /// </summary>
-        /// <param name="playfield">The playfield.</param>
-        /// <returns></returns>
+        /// <param name="playfield">The playfield to be used for generation.</param>
+        /// <returns>An array of ICellView objects.</returns>
         private ICellView[,] CreateCellViews(Playfield playfield)
         { 
             int fieldSize = playfield.PlayfieldSize;
@@ -211,10 +246,11 @@
 
                 for (int j = 0; j < playfield.PlayfieldSize; j++)
                 {
-                    cellViews[i, j] = CreateCellView(playfield[i, j], shouldChangeColor);
+                    cellViews[i, j] = this.CreateCellView(playfield[i, j], shouldChangeColor);
                     shouldChangeColor = !shouldChangeColor;
                 }
             }
+
             return cellViews;
         }
     }
