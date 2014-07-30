448ff8bc1dcbf0b5cf08229f8561a36ceb776ba5
tsigularoff <tsigularoff@gmail.com>
2014-07-30 21:10:13 +0300

    CellFactory.Cs : comments and constants added

--- Cells/CellFactory.cs
+++ Cells/CellFactory.cs
@@ -18,6 +18,26 @@ namespace BattleFiled.Cells
         /// </summary>
         /// <param name="cellType">CellTypes cellType.</param>
         /// <returns>ICell cell.</returns>
+        /// <summary>
+        /// Constant offset for ASCII code that is used for cell rendering.
+        /// </summary>
+        private const int ASCII_VIEW_OFFSET = 48;
+
+        /// <summary>
+        /// Constant for min bomb size.
+        /// </summary>
+        private const int MIN_BOMB_SIZE = 1;
+
+        /// <summary>
+        /// Constant for min bomb size.
+        /// </summary>
+        private const int MAX_BOMB_SIZE = 6;
+
+        /// <summary>
+        /// Cell factory implementation.
+        /// </summary>
+        /// <param name="cellType">Cell type.</param>
+        /// <returns>Required cell.</returns>
         public static ICell CreateCell(CellType cellType)
         {
             //// Uses "lazy initialization".
@@ -30,9 +50,9 @@ namespace BattleFiled.Cells
                         cell.CellView = CellView.Empty;
                         break;
                     case CellType.Bomb:
-                        int bombSize = RandomGenerator.GetRandomNumber(1, 6);
+                        int bombSize = RandomGenerator.GetRandomNumber(MIN_BOMB_SIZE, MAX_BOMB_SIZE);
                         cell = new BombCell(bombSize);
-                        cell.CellView = (CellView)bombSize + 48;
+                        cell.CellView = (CellView)bombSize + ASCII_VIEW_OFFSET;
                         break;
 
                     case CellType.BlownCell:
